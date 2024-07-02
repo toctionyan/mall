7e93266a6967205d4cbcd325c7390b13036d7a06
yantaosheng <1104730926@qq.com>
2024-07-03 00:45:48 +0800

    nihao

--- mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
@@ -30,7 +30,7 @@ public class CmsPrefrenceAreaController {
     @RequestMapping(value = "/listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<CmsPrefrenceArea>> listAll() {
-        System.out.println("1111");
+        System.out.println("111111");
         List<CmsPrefrenceArea> prefrenceAreaList = prefrenceAreaService.listAll();
         return CommonResult.success(prefrenceAreaList);
     }
